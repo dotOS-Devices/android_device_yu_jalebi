7a58eae23cac54e933971b84d15b297fe37dfc7f
MaazMuzammil <maazmuzammil.1426@gmail.com>
2018-05-05 17:21:35 +0000

    Dot-ify

--- dot.mk
+++ dot.mk
@@ -15,13 +15,13 @@
 
 $(call inherit-product, device/yu/jalebi/full_jalebi.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common DotOS stuff.
+$(call inherit-product, vendor/dot/config/common.mk)
 
 # Must define platform variant before including any common things
 TARGET_BOARD_PLATFORM_VARIANT := msm8916
 
-PRODUCT_NAME := lineage_jalebi
+PRODUCT_NAME := dot_jalebi
 BOARD_VENDOR := yu
 PRODUCT_DEVICE := jalebi
 
@@ -30,3 +30,4 @@ PRODUCT_GMS_CLIENTID_BASE := android-micromax
 TARGET_VENDOR_PRODUCT_NAME := YUNIQUE
 TARGET_VENDOR_DEVICE_NAME := YUNIQUE
 PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=YUNIQUE PRODUCT_NAME=YUNIQUE
+DOT_OFFICIAL := true
