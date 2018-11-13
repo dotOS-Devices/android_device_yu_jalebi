90fc7180b8b386693dac5f32efb6a8ea2bf817e7
dev-harsh1998 <harshit.india@linaro.org>
2018-11-13 11:44:28 +0000

    jalebi: Switch to kernel thermal management * MSM simple thermal driver

--- device.mk
+++ device.mk
@@ -239,10 +239,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl
 
-# Thermal
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
-
 # Trust HAL
 PRODUCT_PACKAGES += \
     vendor.lineage.trust@1.0-service
